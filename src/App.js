b18b7bcd1f075b6ce0f3ab0ec366815ef2232292
Michsow <94047110+Michsow@users.noreply.github.com>
2023-05-25 11:41:00 +0200

    poof

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const [getTaskList, setTaskList] = useState([]);
+const jobCollectionRef = collection(db, "tasks");
+ 
+  useEffect(()=>{
+    const getTasks = async () => {
+      const data = await getDocs(jobCollectionRef);
+      setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    getTasks();
+  },[])
+
 function App() {
   return (
     <div className="App">
