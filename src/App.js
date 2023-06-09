adfc60a6d19322f37d2af345bf4c90d51edc8765
KanashiQba <94380630+KanashiQba@users.noreply.github.com>
2023-06-09 15:53:22 +0200

    connection I guess

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
 import logo from './logo.svg';
 import './App.css';
 
+// Initialize your Firebase app
+const firebaseConfig = {
+  apiKey: "AIzaSyCSO6bRVvBno33Xmc-i2qG_EHNWTl9iXag",
+  authDomain: "globalnet-8386f.firebaseapp.com",
+  projectId: "globalnet-8386f",
+  storageBucket: "globalnet-8386f.appspot.com",
+  messagingSenderId: "926314328592",
+  appId: "1:926314328592:web:28f67b1d5df5b38bfb5095",
+  measurementId: "G-MKFB509T84"
+};
 
-const [getTaskList, setTaskList] = useState([]);
-const jobCollectionRef = collection(db, "tasks");
- 
-  useEffect(()=>{
-    const getTasks = async () => {
-      const data = await getDocs(jobCollectionRef);
-      setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-    }
-    getTasks();
-  },[])
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp); // Get the Firestore instance
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    const jobCollectionRef = collection(db, 'tasks');
+
+    const getTasks = async () => {
+      const data = await getDocs(jobCollectionRef);
+      setTaskList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getTasks();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+         <a>
+          CHUJ
         </a>
       </header>
     </div>
